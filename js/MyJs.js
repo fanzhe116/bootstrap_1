5eba73265f628c6c2465fe4dd9460aa60e099277
fanzhe116 <18340809646@163.com>
2017-10-06 17:15:14 +0800

    first

---
+++ js/MyJs.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/9/17.
+ */
+<!--导航跳转插件-->
+$(function () {
+    $('.nav').singlePageNav({
+        offset:70
+    });
+    //小屏幕点击关闭菜单
+    $('.navbar-collapse').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+    new WOW
+})
